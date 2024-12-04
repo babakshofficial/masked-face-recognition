052caa825c12f580d27a10748b70901acd7df697
Babak <babakshofficial@yahoo.co.uk>
2024-12-04 13:46:31 +0330

    Create CBAM_module.py

---
+++ CBAM_module.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Lambda, Multiply
+from tensorflow.keras.layers import Reshape, Conv2D, GlobalMaxPooling2D, Activation, add
+
+
+def channel_attention(input_feature, ratio=8):
+    channel = input_feature.shape[-1]
+
+    shared_layer_one = Dense(channel // ratio,
+                             activation='relu',
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+    shared_layer_two = Dense(channel,
+                             kernel_initializer='he_normal',
+                             use_bias=True,
+                             bias_initializer='zeros')
+
+    avg_pool = GlobalAveragePooling2D()(input_feature)
+    avg_pool = Reshape((1, 1, channel))(avg_pool)
+    assert avg_pool.shape[1:] == (1, 1, channel)
+    avg_pool = shared_layer_one(avg_pool)
+    assert avg_pool.shape[1:] == (1, 1, channel // ratio)
+    avg_pool = shared_layer_two(avg_pool)
+    assert avg_pool.shape[1:] == (1, 1, channel)
+
+    max_pool = GlobalMaxPooling2D()(input_feature)
+    max_pool = Reshape((1, 1, channel))(max_pool)
+    assert max_pool.shape[1:] == (1, 1, channel)
+    max_pool = shared_layer_one(max_pool)
+    assert max_pool.shape[1:] == (1, 1, channel // ratio)
+    max_pool = shared_layer_two(max_pool)
+    assert max_pool.shape[1:] == (1, 1, channel)
+
+    cbam_feature = Activation('sigmoid')(add([avg_pool, max_pool]))
+
+    return Multiply()([input_feature, cbam_feature])
+
+
+def spatial_attention(input_feature):
+    kernel_size = 7
+
+    avg_pool = Lambda(lambda x: tf.keras.backend.mean(x, axis=3, keepdims=True))(input_feature)
+    max_pool = Lambda(lambda x: tf.keras.backend.max(x, axis=3, keepdims=True))(input_feature)
+    concat = tf.keras.layers.Concatenate(axis=3)([avg_pool, max_pool])
+    cbam_feature = Conv2D(filters=1,
+                          kernel_size=kernel_size,
+                          strides=1,
+                          padding='same',
+                          activation='sigmoid',
+                          kernel_initializer='he_normal',
+                          use_bias=False)(concat)
+
+    return Multiply()([input_feature, cbam_feature])
